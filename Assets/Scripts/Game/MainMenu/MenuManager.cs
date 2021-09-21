107d7e121ea0c2fe81ae13b0539bb8ffca5d8749
Koga <79469238+agarangga55@users.noreply.github.com>
2021-09-21 08:47:43 +0700

    about UI + hyperlink to maps,socials,grabfood + 2 menu list examples

--- Assets/Scripts/Game/MainMenu/MenuManager.cs
+++ Assets/Scripts/Game/MainMenu/MenuManager.cs
@@ -7,12 +7,16 @@ public class MenuManager : MonoBehaviour
     public GameObject SettingsBox;
     public GameObject MenuListBox;
     public GameObject ExitBox;
+    public GameObject AboutBox;
+    public GameObject OrderBox;
     // Start is called before the first frame update
     void Start()
     {
         SettingsBox.SetActive(false);
         MenuListBox.SetActive(false);
         ExitBox.SetActive(false);
+        AboutBox.SetActive(false);
+        OrderBox.SetActive(false);
     }
 
     // Update is called once per frame
@@ -50,8 +54,36 @@ public class MenuManager : MonoBehaviour
     {
         ExitBox.SetActive(false);
     }
+    public void OpenAbout()
+    {
+        AboutBox.SetActive(true);
+    }
+    public void CloseAbout()
+    {
+        AboutBox.SetActive(false);
+    }
     public void ExitGame()
     {
         Application.Quit();
+    }
+    public void OpenBuy()
+    {
+        OrderBox.SetActive(true);
+    }
+    public void CloseBuy()
+    {
+        OrderBox.SetActive(false);
+    }
+    public void OpenMaps()
+    {
+        Application.OpenURL("https://www.google.com/maps/place/Aljava+Cafe/@-6.8079354,110.8404192,15z/data=!4m5!3m4!1s0x0:0x695b061a6027cab2!8m2!3d-6.8080939!4d110.8403623");
+    }
+    public void OpenSocials()
+    {
+        Application.OpenURL("https://www.instagram.com/aljavacafe/");
+    }
+    public void OpenGrabFood()
+    {
+        Application.OpenURL("https://food.grab.com/id/en/restaurant/aljava-cafe-demaan-delivery/6-C2E1RFKATUKJJN");
     }
 }
